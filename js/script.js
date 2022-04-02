9bd63273c5566efc7dc8c5edb734bbba135ff055
ph1y <94645960+ph1y@users.noreply.github.com>
2022-04-02 21:37:48 +0200

    Allow resizing of grid according to player input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
-let gridContainer = document.querySelector(".grid-container");
+function generateGrid(squaresPerSide) {
+    let gridContainer = document.querySelector(".grid-container");
+    gridContainer.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
 
-for (let i = 0; i < 16 * 16; i++) {
-    gridContainer.appendChild(document.createElement("div"));
+    if (gridContainer.firstChild) {
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.lastChild);
+        }
+    };
+
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+        gridContainer.appendChild(document.createElement("div"));
+    };
+
+    const gridDivs = Array.from(document.querySelectorAll(".grid-container div"));
+    gridDivs.forEach(div => div.addEventListener("mouseenter", addHighlight));
 };
+
+function addHighlight(event) {
+    event.target.classList.add("highlighted");
+};
+
+function clearGrid() {
+    let squaresPerSide = parseInt(prompt("How many squares should the grid have?"));
+
+    if (squaresPerSide <= 100) {
+        generateGrid(squaresPerSide);
+    } else {
+        alert("The maximum amount of squares is 100. Please try again.");
+        clearGrid();
+    };
+};
+
+const button = document.querySelector("button");
+button.addEventListener("click", clearGrid);
+
+generateGrid(16);
